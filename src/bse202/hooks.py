8739f8156bf1cf9276e54b755a7e4562241d05ed
d3rpp <270099694@yoobeestudent.ac.nz>
2024-05-07 11:02:09 +1200

    Set-Cookie header only sent if cookie needs updating

--- src/bse202/hooks.py
+++ src/bse202/hooks.py
@@ -2,13 +2,13 @@ from .lib import generate_default_cookie
 
 from flask import Flask, Response, g, request
 from itsdangerous import URLSafeSerializer
-from os import environ, abort
+from os import environ, _exit
 
 secret_key = environ.get("SECRET_KEY")
 
 if secret_key is None:
     print("SECRET_KEY not set")
-    abort()
+    _exit(1)
 
 # We've already checked that SECRET_KEY is set
 auth_serializer = URLSafeSerializer(secret_key)  # type: ignore
@@ -23,17 +23,20 @@ def register_hooks(app: Flask):
 
         auth_cookie = request.cookies.get("auth")
         if auth_cookie is not None:
+            g.original_token = auth_cookie
             g.token = auth_serializer.loads(auth_cookie)
         else:
-            g.token = generate_default_cookie(ip=g.ip, authenticated=False)
+            new_cookie = generate_default_cookie(ip=g.ip, authenticated=False)
+            g.token = new_cookie
 
     # Post-Request Middleware
     @app.after_request
     def _(response: Response) -> Response:
         if "token" in g:
-            response.set_cookie(
-                key="auth", value=auth_serializer.dumps(g.token), expires=1746524701
-            )
-
-        response.headers.set("X-Test-Header", g.get("ua"))
+            # Only update cookie if change occured
+            serialised_cookie = auth_serializer.dumps(g.token)
+            if "original_token" not in g or serialised_cookie != g.original_token:
+                response.set_cookie(
+                    key="auth", value=auth_serializer.dumps(g.token), expires=1746524701
+                )
         return response
