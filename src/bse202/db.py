48822302c348a00d7a9118b1752dce42b5523300
d3rpp <270099694@yoobeestudent.ac.nz>
2024-05-10 09:34:33 +1200

    sqlite

---
+++ src/bse202/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+from flask import g
+
+DB_LOCATION = "./database.db"
+
+def get_db():
+	db = getattr(g, "_db", None)
+	if db is None:
+		db = g._db = sqlite3.connect(DB_LOCATION)
+	return db
