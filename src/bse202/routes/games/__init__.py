13236374d3d5bc3afc2bb87187dc3698ae5ddb75
d3rpp <270099694@yoobeestudent.ac.nz>
2024-05-21 11:17:00 +1200

    added games view page, with HTMX

--- src/bse202/routes/games/__init__.py
+++ src/bse202/routes/games/__init__.py
@@ -8,5 +8,6 @@ Prefix: `/games`
 from .blueprint import games_blueprint
 from .index import games_root
 from .storepage import game_store_page
+from .storepage_reviews import game_storepage_reviews
 
 __all__ = ["games_blueprint"]
---
+++ src/bse202/routes/games/storepage_reviews.py
@@ -0,0 +1,13 @@
+from flask import render_template, g
+from sqlite3 import DatabaseError
+from time import sleep
+
+from .blueprint import games_blueprint
+from ...db import get_db
+
+
+@games_blueprint.get("/<int:game_id>/reviews")
+def game_storepage_reviews(game_id: int):
+    # artificially delay to let lazy loading look nice
+	sleep(1.5)
+	return render_template(f"{g.template_prefix}games/snippets/reviews.html")
