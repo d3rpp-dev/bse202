32d194e3cfefe7a9b5c2ce2a6fdd0e34bd8d62bb
SpursFan21 <duncanfrederic@hmail.com>
2024-06-17 08:52:25 +1200

    p

--- src/bse202/templates/elements-function/searchbar.py
+++ src/bse202/templates/elements-function/searchbar.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, jsonify, render_template
+
 app = Flask(__name__)
 
 games = ["Valorant", "Fall Guys", "DOTA2"]
 
-@app.route('/')
+
+@app.route("/")
 def index():
-   return render_template('searchbar.html')
+    return render_template("searchbar.html")
 
-@app.route('/search')
+
+@app.route("/search")
 def search():
-   query = request.args.get('query', '')
-   if query:
-       results = [game for game in games if query.lower() in game.lower()]
-   else:
-       results = []
-   return jsonify(results)
+    query = request.args.get("query", "")
+    if query:
+        results = [game for game in games if query.lower() in game.lower()]
+    else:
+        results = []
+    return jsonify(results)
 
-if __name__ == '__main__':
-   app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
