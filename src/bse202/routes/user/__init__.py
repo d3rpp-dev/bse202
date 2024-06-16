3a2e9d88b2600ae66d0354d0114f45e8392eb528
d3rpp <270099694@yoobeestudent.ac.nz>
2024-06-17 11:26:00 +1200

    fixed all of the links now we need to just add all of the database stuff

--- src/bse202/routes/user/__init__.py
+++ src/bse202/routes/user/__init__.py
@@ -1,7 +1,7 @@
 from .blueprint import user_blueprint
 
 from .index import self
-from .by_id import user_home
+from .by_id import account
 from .top_up_credit import top_up_credit
 from .update_top_up_option import update_top_up_option
 from .voucher import voucher
--- src/bse202/routes/user/index.py
+++ src/bse202/routes/user/index.py
@@ -15,7 +15,7 @@ def self():
     if "token" in g:
         user_id = g.token["user_id"]
         if user_id is not None:
-            return redirect(url_for("user.user_home", user_id=user_id)), 302
+            return redirect(url_for("user.account", user_id=user_id)), 302
         else:
             return redirect(url_for("auth.login")), 307
     else:
